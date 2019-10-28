ab60bacdf1f134bfe42281675a646d1210bc85eb
sudhons <sudhons@gmail.com>
2019-10-28 21:08:11 +0100

    implement getting uniques from 2 lists

--- DuplicateLists.java
+++ DuplicateLists.java
@@ -32,4 +32,41 @@ public class DuplicateLists {
 
     return result;
   }
+
+  public static <T extends Comparable<? super T>> List<T> getUniques(List<T> list1, List<T> list2) {
+    List<T> result = new ArrayList<>();
+    int index1 = 0;
+    int index2 = 0;
+    final int list1Size = list1.size();
+    final int list2Size = list2.size();
+
+    Collections.sort(list1);
+    Collections.sort(list2);
+
+    while (index1 < list1Size && index2 < list2Size) {
+      int compareResult = list1.get(index1).compareTo(list2.get(index2));
+
+      if (compareResult < 0) {
+        result.add(list1.get(index1));
+        index1 += 1;
+      } else if (compareResult > 0) {
+        result.add(list2.get(index2));
+        index2 += 1;
+      } else {
+        index1 += 1;
+        index2 += 1;
+      }
+    }
+
+    while (index1 < list1Size) {
+      result.add(list1.get(index1));
+      index1 += 1;
+    }
+    while (index2 < list2Size) {
+      result.add(list2.get(index2));
+      index2 += 1;
+    }
+
+    return result;
+  }
 }
